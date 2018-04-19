24de579c90fadd757c4f3bc24f92db4d36dea6df
Wendy Braun <wendocalrissian@gmail.com>
2018-04-19 01:39:18 +0000

    Added new font and additional styling

--- chatty_app/src/Navigation.jsx
+++ chatty_app/src/Navigation.jsx
@@ -4,9 +4,9 @@ export default class Navigation extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty Cantina</a>
-        { (this.props.onlineUsers > 1) && <p className="navbar-usersOnline">{this.props.onlineUsers} users online</p> }
-        { (this.props.onlineUsers === 1) && <p className="navbar-usersOnline">{this.props.onlineUsers} user online</p> }
+        <a href="/" className="navbar-brand">CHATTY CANTINA</a>
+        { (this.props.onlineUsers > 1) && <p className="navbar-usersOnline">{this.props.onlineUsers} USERS ONLINE</p> }
+        { (this.props.onlineUsers === 1) && <p className="navbar-usersOnline">{this.props.onlineUsers} USER ONLINE</p> }
       </nav>
     );
   }
